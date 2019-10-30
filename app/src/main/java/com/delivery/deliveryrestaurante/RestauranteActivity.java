fa576f753451605469ba47f0b340f1aff7f20a81
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-10-30 12:40:19 -0500

    Menu en la vista restaurante + refactor

--- app/src/main/java/com/delivery/deliveryrestaurante/RestauranteActivity.java
+++ app/src/main/java/com/delivery/deliveryrestaurante/RestauranteActivity.java
@@ -8,6 +8,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class RestauranteActivity extends AppCompatActivity {
@@ -20,4 +22,25 @@ public class RestauranteActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_restaurante, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_restaurante) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
