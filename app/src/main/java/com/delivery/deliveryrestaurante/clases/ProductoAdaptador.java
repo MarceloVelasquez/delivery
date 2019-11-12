dc763fc9b53e591681a14125c81226c17bcbf0c9
JavCho23 <javier120699lili@gmail.com>
2019-11-11 23:42:34 -0500

    cargada de imagen en el producto

--- app/src/main/java/com/delivery/deliveryrestaurante/clases/ProductoAdaptador.java
+++ app/src/main/java/com/delivery/deliveryrestaurante/clases/ProductoAdaptador.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.delivery.deliveryrestaurante.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -38,14 +40,15 @@ public class ProductoAdaptador extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
         Producto producto = (Producto) getItem(position);
 
         convertView = LayoutInflater.from(contexto).inflate(R.layout.item_producto, null);
         TextView nombre = convertView.findViewById(R.id.item_nombre);
         TextView categoria = convertView.findViewById(R.id.item_categoria);
         TextView precio = convertView.findViewById(R.id.item_precio);
+        ImageView imagen = convertView.findViewById(R.id.item_imagen);
 
+        Picasso.get().load(producto.getImagen()).into(imagen);
         nombre.setText(producto.getNombre());
         categoria.setText(producto.getCategoria());
         precio.setText(producto.getPrecioEnSoles());
